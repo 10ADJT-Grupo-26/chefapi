32570933ae7231662953fe26c447829432239314
mateus barbeiro <mateusgarcia2001@gmail.com>
2025-10-05 14:24:47 -0300

    chore: set up initial spring boot project structure

---
+++ src/main/java/com/twentysixcore/chefapi/ChefapiApplication.java
@@ -0,0 +1,13 @@
+package com.twentysixcore.chefapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twentysixcore/chefapi/ChefapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twentysixcore.chefapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
